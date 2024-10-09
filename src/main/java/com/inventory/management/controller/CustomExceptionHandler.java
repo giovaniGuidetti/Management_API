4e62db2b1115daea7a012a7601c89fd84b60be44
Giovani <giovanibguidetti@gmail.com>
2024-10-08 23:15:16 -0300

    removing things that were not being used

--- src/main/java/com/inventory/management/controller/CustomExceptionHandler.java
+++ src/main/java/com/inventory/management/controller/CustomExceptionHandler.java
@@ -7,11 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestControllerAdvice
